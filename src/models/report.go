e8c5ceb217f839e4ac956cf261836d83a13d1bc4
sahatsawats <sae.sahatsawat@gmail.com>
2025-03-18 16:00:24 +0700

    [ref] change the approach about how to map a data to DatabaseReport models

--- src/models/report.go
+++ src/models/report.go
@@ -12,17 +12,12 @@ type Database struct {
 	DatabaseName string
 	NumOfTable int
 	Tables []Table
-	Indexes []Index
 }
 
 
 type Table struct {
 	TableName string
+	Indexes []string
 	Rows int
-	Size float32
-}
-
-type Index struct {
-	IndexName string
 	Size float32
 }
